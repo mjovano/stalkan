2560845038555b21b10f4bb00103c0b8a54b0f1f
mjovano <127967109+mjovano@users.noreply.github.com>
2024-08-18 23:07:55 +0200

    v1 fix2

--- pages/layout.tsx
+++ pages/layout.tsx
@@ -1,3 +1,4 @@
+/*
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -19,4 +20,4 @@ export default function RootLayout({
       <body className={inter.className}>{children}</body>
     </html>
   );
-}
+} */
